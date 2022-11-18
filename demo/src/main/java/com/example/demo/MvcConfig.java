6822d5bdc15261a0809608b906cfe460b69745a9
yeshunator <80420202+yeshunator@users.noreply.github.com>
2022-11-18 17:44:50 -0400

    crud de subir imagen v1

---
+++ demo/src/main/java/com/example/demo/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		
+		registry.addResourceHandler("/uploads/**").addResourceLocations("src/main/resources/static/uploads/");
+	}
+
+}
