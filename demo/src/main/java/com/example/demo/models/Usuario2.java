a4e61afa1e0511b1c7459d9cb4fc14136c305ca2
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-12-08 16:18:00 -0500

    construcion de login mysql

---
+++ demo/src/main/java/com/example/demo/models/Usuario2.java
@@ -0,0 +1,35 @@
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario2 {
+	
+	@Id
+	private int id;
+	private String nombre;
+	private String clave;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	
+}
